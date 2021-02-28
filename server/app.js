3af0e1551dba5f8b575ffe82aa6ce6cd9274772e
Stelian Grosu <stelian.email@yahoo.com>
2021-02-28 18:22:59 -0500

    s1

--- server/app.js
+++ server/app.js
@@ -9,7 +9,7 @@ const path = require('path');
 const cors = require('cors');
 
 mongoose
-  .connect('mongodb://localhost/movies-dev', { useCreateIndex: true, useNewUrlParser: true, useUnifiedTopology: true })
+  .connect('dotenv.mongodb', { useCreateIndex: true, useNewUrlParser: true, useUnifiedTopology: true })
   .then(x => {
     console.log(`Connected to Mongo! Database name: "${x.connections[0].name}"`);
   })
