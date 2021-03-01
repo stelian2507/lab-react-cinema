4a1abe22c0298834e0e6937ee0115d38f05927d1
Stelian Grosu <stelian.email@yahoo.com>
2021-02-28 20:25:43 -0500

    s3

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const { default: MovieInfo } = require('../../client/src/components/movieInfo');
 const router = express.Router();
 
 /* GET home page */
@@ -7,5 +8,22 @@ router.get('/', (req, res) => {
   res.json({ message: 'index' });
 });
 
+router.get('/movies', (req,res) => {
+  Movie.find()
+.then(movies) => res.json(movies))
+.catch(error) =>
+console.log('error 2'))
+})
+
+router.get('/movies/:id'), (req, res) =>
+{
+  Movie.findById(req,prams.id)
+  .then((movies) => res.json(movies))
+  .catch((error) => 
+  console.log('error 3')
+})
+})
+
 module.exports = router;
+
 
